c6088e9a3f2cba4d22403fe6371bd90d8fdc4ce6
bemijonathan <bemijonathan@gmail.com>
2021-04-13 17:24:22 +0100

    more fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/api/v1/users', createProxyMiddleware({ target: 'https://kliqr-user-service.herokuapp.com', changeOrigin: false }));
-app.use('/api/v1/transactions', createProxyMiddleware({ target: 'https://kliqr-transactions-service.herokuapp.com', changeOrigin: false }));
+app.use('/api/v1/users', createProxyMiddleware({ target: 'https://kliqr-user-service.herokuapp.com', changeOrigin: true }));
+app.use('/api/v1/transactions', createProxyMiddleware({ target: 'https://kliqr-transactions-service.herokuapp.com', changeOrigin: true }));
 
 app.listen(process.env.PORT || 3000);
