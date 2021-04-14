9931e3f208cc2e03344459750d52486c3b867bfc
bemijonathan <bemijonathan@gmail.com>
2021-04-14 14:59:26 +0100

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
 
 app.use('/api/v1/users', createProxyMiddleware({ target: 'https://kliqr-user-service.herokuapp.com', changeOrigin: true }));
 app.use('/api/v1/transactions', createProxyMiddleware({ target: 'https://kliqr-transactions-service.herokuapp.com', changeOrigin: true }));
 
-app.listen(process.env.PORT || 3000);
+// app.use('/api/v1/users', createProxyMiddleware({ target: 'http://localhost:4500', changeOrigin: true }));
+// app.use('/api/v1/transactions', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
+
+app.listen(process.env.PORT || 4000);
